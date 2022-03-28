6e3cae2e28154e54b0e7f6ba516ac22dc6d7427a
NTT123 <9960521+NTT123@users.noreply.github.com>
2022-03-28 15:27:32 +0000

    Increase batchsize, reduce segment size.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 version = "v1"
 resblock_kind = "1"
-batch_size = 16
+batch_size = 32
 learning_rate = 0.0002
 adam_b1 = 0.8
 adam_b2 = 0.99
@@ -9,7 +9,7 @@ upsample_rates = [10, 5, 3, 2]
 upsample_kernel_sizes = [20, 10, 6, 4]
 resblock_kernel_sizes = [3, 7, 11]
 resblock_dilation_sizes = [[1, 3, 5], [1, 3, 5], [1, 3, 5]]
-segment_size = 9600
+segment_size = 300*28
 num_mels = 80
 num_freq = 1201
 n_fft = 1200
